b39c5118d281f14a8786ceb18b6b4fd71baeca16
madebysag <abdulgafarsulaimon3@gmail.com>
2025-11-07 09:33:59 +0100

    Worked on the porfolio - Added some projects

--- js/playground.js
+++ js/playground.js
@@ -1,29 +1,39 @@
 import ThemeSwitcher from "./ThemeSwitcher.js"
 
-
 const body = document.querySelector("body")
 const modalBtn = document.querySelector("button.side-modal")
 const modal = document.querySelector("aside")
-// const themeSwitcher = document.querySelector(".theme-switcher")
-const links = [...document.querySelectorAll("aside > nav > ul > li a")]
+const links = [...document.querySelectorAll("aside > nav > ul li a")]
 
+const iframe = document.querySelector("iframe#main")
+const briefContainer = document.querySelector("p#brief")
+const titleContainer = document.querySelector("header#title")
 
 
 const themeSwitcher = new ThemeSwitcher(document.querySelector(".theme-switcher"), body)
 
 modalBtn.addEventListener("click", (e) => {
 	modal.classList.toggle("open")
-
-
-	// hamburger.classList.toggle("active")
-
-	// hamburgerList.forEach((li, index) => {
-	// 	li.style.animationDelay = `${index * 0.1}s`
-	// })
-
 })
 
-// themeSwitcher.addEventListener("click", (e) => {
-// 	body.classList.toggle("dark-theme")
-// 	themeSwitcher.innerText == "Light theme" ? themeSwitcher.innerText = "Dark theme" : themeSwitcher.innerText = "Light theme"
-// })
+links.forEach(link => {
+	link.addEventListener("click", e => {
+
+		changeView(
+			e.target.innerText,
+			e.target.dataset.brief,
+			e.target.getAttribute("href")
+			)
+
+		links.forEach( item => item.classList.remove("active"))
+		e.target.classList.add("active")
+
+		e.preventDefault()
+	})
+})
+
+function changeView(title, brief, src) {
+	iframe.src = src
+	briefContainer.innerHTML = brief
+	titleContainer.innerText = "Playground | " + title
+}
